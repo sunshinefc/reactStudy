08092c40d22bc2437cb55ddaf02034f090990ebc
feicongcong <1075025698@qq.com>
2019-04-17 10:31:13 +0800

    'router'

--- goodGril/React语法/router.jsx
+++ goodGril/React语法/router.jsx
@@ -3,7 +3,22 @@ window.location.href='http://www.baidu.com';
 history.back();
 
 //hash路由
-window.location='#hash';
+window.location.href='#hash';
 window.onhashchange=function(){
-	console.log('current hash:')
+	console.log('current hash:',window.location.hash);
+}
+
+//h5路由
+//推进一个状态
+history.pushState('name','title','/path');
+
+//替换一个状态
+history.replaceState('name','title','/path');
+
+//popstate
+window.onpopstate=function(){
+	console.log(window.location.href);
+	console.log(window.location.pathname);
+	console.log(window.location.hash);
+	console.log(window.location.search);
 }
--- goodGril/admin-v2-fe/src/app.jsx
+++ goodGril/admin-v2-fe/src/app.jsx
@@ -1,99 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-class Component extends React.Component{
-	//构造函数
+
+class A extends React.Component{
 	constructor(props){
 		super(props)
-		this.state={
-			data:'Old State'
-		}
-		console.log('初始化数据：constructor');
 	}
-	//组件将要加载
-	componentWillMount(){
-		console.log('componentWillMount')
-	}
-	//组件加载完成
-	componentDidMount(){
-		console.log('componentDidMount')
-	}
-	//将要接受父组件传来的props
-	componentWillReceiveProps(){
-		console.log('componentWillReceiveProps')
-	}
-	//子组件是不是应该更新
-	shouldComponentUpdate(){
-		console.log('shouldComponentUpdate')
-		return true;
-	}
-	//组件将要更新
-	componentWillUpdate(){
-		console.log('componentWillUpdate')
-	}
-	//组件更新完成
-	componentDidUpdate(){
-		console.log('componentDidUpdate')
-	}
-	// 组件即将销毁
-	componentWillUnmount(){
-		console.log('componentWillUnmount')
-	}
-	//处理点击事件
-
-	handleClick(){
-		console.log('更新数据：')
-		this.setState({
-			data:'New State'
-		});
-	}
-	//渲染
 	render(){
-		console.log('render')
-		return (
-			<div>
-				<div>Props:{this.props.data}</div>
-				<button onClick={()=>{this.handleClick()}}>更新组件</button>
-			</div>
-		)
+		return <div>Component A</div>
 	}
 }
 
-class App extends React.Component{
+class B extends React.Component{
 	constructor(props){
 		super(props)
-		this.state={
-			data:'Old props',
-			hasChild:true
-		}
-		console.log('初始化数据：constructor');
-	}
-	onPropsChange(){
-		console.log('更新props')
-		this.setState({
-			data:'New props'
-		})
-	}
-	onDestoryChild(){
-		console.log('干掉子组件：')
-		this.setState({
-			hasChild:false
-		})
-
 	}
 	render(){
-		return(
-			<div>
-				{
-					this.state.hasChild?<Component data={this.state.data}/>:null
-				}
-				
-				<button onClick={()=>{this.onPropsChange()}}>改变Props</button>
-				<button onClick={()=>{this.onDestoryChild()}}>干掉子组件</button>
-			</div>
-		)
+		return <div>Component B</div>
 	}
 }
-
+class Wrapper extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return (<div>
+			{this.props.children}
+		</div>)
+	}
+}
 
 ReactDOM.render(
 	<App/>,
