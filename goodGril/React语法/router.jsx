a9a86c8c4a5895e2ec44b685403808e42fbf9b06
feicongcong <1075025698@qq.com>
2019-04-17 16:01:12 +0800

    'react-route'

--- goodGril/React语法/router.jsx
+++ goodGril/React语法/router.jsx
@@ -15,10 +15,77 @@ history.pushState('name','title','/path');
 //替换一个状态
 history.replaceState('name','title','/path');
 
-//popstate
+//popstate  在window上监听onpopstate
 window.onpopstate=function(){
 	console.log(window.location.href);
 	console.log(window.location.pathname);
 	console.log(window.location.hash);
 	console.log(window.location.search);
 }
+
+//react-router
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+
+class A extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return (<div>
+			Component A
+			
+			<Switch>
+			<Route exact path={`${this.props.match.path}`} render={(route)=>{
+					return <div>当前组件是不带参数的A</div>
+			}}/>
+			<Route path={`${this.props.match.path}/sub`} render={(route)=>{
+				return <div>当前组件是sub</div>
+			}}/>
+			<Route path={`${this.props.match.path}/:id`} render={(route)=>{
+				return <div>当前组件是带参数的A,参数是：{route.match.params.id}</div>
+			}}/>
+
+			</Switch>
+			</div>)
+	}
+}
+
+class B extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return <div>Component B</div>
+	}  
+}
+class Wrapper extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){
+		return (<div>
+			<Link to="/a">组件A</Link>
+			<br/>
+			<Link to="/a/123">带参数组件A</Link>
+			<br/>
+			<Link to="/b">组件B</Link>
+			<br/>
+			<Link to="/a/sub">/a/sub</Link>
+			{this.props.children}
+		</div>);
+	}
+}
+
+ReactDOM.render(
+	<Router>
+		<Wrapper>
+			<Route path="/a" component={A}/>
+			<Route path="/b" component={B}/>
+		</Wrapper>
+	</Router>,
+	document.getElementById('app')
+)
--- goodGril/admin-v2-fe/src/app.jsx
+++ goodGril/admin-v2-fe/src/app.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 
 class A extends React.Component{
 	constructor(props){
 		super(props)
 	}
 	render(){
-		return <div>Component A</div>
+		return (<div>
+			Component A
+			
+			<Switch>
+			<Route exact path={`${this.props.match.path}`} render={(route)=>{
+					return <div>当前组件是不带参数的A</div>
+			}}/>
+			<Route path={`${this.props.match.path}/sub`} render={(route)=>{
+				return <div>当前组件是sub</div>
+			}}/>
+			<Route path={`${this.props.match.path}/:id`} render={(route)=>{
+				return <div>当前组件是带参数的A,参数是：{route.match.params.id}</div>
+			}}/>
+
+			</Switch>
+			</div>)
 	}
 }
 
@@ -16,7 +33,7 @@ class B extends React.Component{
 	}
 	render(){
 		return <div>Component B</div>
-	}
+	}  
 }
 class Wrapper extends React.Component{
 	constructor(props){
@@ -24,12 +41,24 @@ class Wrapper extends React.Component{
 	}
 	render(){
 		return (<div>
+			<Link to="/a">组件A</Link>
+			<br/>
+			<Link to="/a/123">带参数组件A</Link>
+			<br/>
+			<Link to="/b">组件B</Link>
+			<br/>
+			<Link to="/a/sub">/a/sub</Link>
 			{this.props.children}
-		</div>)
+		</div>);
 	}
 }
 
 ReactDOM.render(
-	<App/>,
+	<Router>
+		<Wrapper>
+			<Route path="/a" component={A}/>
+			<Route path="/b" component={B}/>
+		</Wrapper>
+	</Router>,
 	document.getElementById('app')
 )
