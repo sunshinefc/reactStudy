f12d9a7b22359cc45b596795778584659f37de2b
feicongcong <1075025698@qq.com>
2019-04-24 12:26:48 +0800

    'home'

--- goodGril/admin-v2-fe/src/page/home/index.jsx
+++ goodGril/admin-v2-fe/src/page/home/index.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import PageTitle from "component/page-title/index.jsx";
-import './index.css'
+import {Link} from  "react-router-dom";
+import './index.scss'
 class Home extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			userCount:'111',
+			productCount:'1112',
+			orderCount:'2222'
+		}
+	}
 	render(){
 		return (
 			<div id="page-wrapper">
 				<PageTitle title="首页"/>
 				<div className="row">
-					<div className="col-md-12">
-						body
+					<div className="col-md-4">
+						<Link to='/user' className='color-box brown'>
+							<p className="count">{this.state.userCount}</p>
+							<p className="desc">
+								<i className="fa fa-user-o"></i>
+								<span>用户总数</span>
+							</p>
+						</Link>
+					</div>
+					<div className="col-md-4">
+						<Link to='/user' className='color-box green'>
+							<p className="count">{this.state.productCount}</p>
+							<p className="desc">
+								<i className="fa fa-list"></i>
+								<span>商品总数</span>
+							</p>
+						</Link>
+					</div>
+					<div className="col-md-4">
+						<Link to='/user' className='color-box blue'>
+							<p className="count">{this.state.orderCount}</p>
+							<p className="desc">
+								<i className="fa fa-check-square-o"></i>
+								<span>订单总数</span>
+							</p>
+						</Link>
 					</div>
 				</div>
 			</div>
