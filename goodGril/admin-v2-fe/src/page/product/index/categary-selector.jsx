cb6da61776c98987adc38e394a44631cff96a097
feicongcong <1075025698@qq.com>
2019-04-25 14:38:57 +0800

    'addprpduct'

---
+++ goodGril/admin-v2-fe/src/page/product/index/categary-selector.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MUtil from 'util/mm.jsx';
+import Product from 'service/product-service.jsx';
+
+const _mm=new MUtil();
+const _product=new Product();
+class CategorySelect extends React.Component{
+	render(){
+		return (
+			<div></div>
+		);
+	}
+}
+export default CategorySelect;
--- goodGril/admin-v2-fe/src/page/product/index/save.jsx
+++ goodGril/admin-v2-fe/src/page/product/index/save.jsx
@@ -3,6 +3,10 @@ import MUtil from 'util/mm.jsx';
 import Product from 'service/product-service.jsx';
 import PageTitle from 'component/page-title/index.jsx';
 
+import CategorySelect from 'product/index/categary-selector.jsx';
+
+
+
 const _mm=new MUtil();
 const _product=new Product();
 class ProductSave extends React.Component{
@@ -24,20 +28,52 @@ class ProductSave extends React.Component{
 				    </div>
 				  </div>
 				  <div className="form-group">
-				    <label  className="col-md-2 control-label">商品分类</label>
+				    <label  className="col-md-2 control-label">所属分类</label>
+				    <CategorySelect />
+				   
+				  </div>
+				  <div className="form-group">
+				    <label  className="col-md-2 control-label">商品价格</label>
+				    <div className="col-md-3">
+				        <div className="input-group">
+						  <input type="number" className="form-control" placeholder="价格"/>
+						  <span className="input-group-addon">元</span>
+						</div>
+				    </div>
+				  </div>
+				  
+				  <div className="form-group">
+				    <label  className="col-md-2 control-label">商品库存</label>
+				    <div className="col-md-3">
+				    	<div className="input-group">
+						  <input type="number" className="form-control" placeholder="库存"/>
+						  <span className="input-group-addon">件</span>
+						</div>
+				      
+				    </div>
+				  </div>
+				  <div className="form-group">
+				    <label  className="col-md-2 control-label">商品图片</label>
 				    <div className="col-md-10">
-				      <select name=""  className="form-control col-md-5">
-				      	<option value="">请选择一级分类</option>
-				      </select>
-				      <select name=""  className="form-control col-md-4">
-				      	<option value="">请选择二级分类</option>
-				      </select>
+				    	<div className="input-group">
+						  xxx
+						</div>
+				      
+				    </div>
+				  </div>
+				  <div className="form-group">
+				    <label  className="col-md-2 control-label">商品详情</label>
+				    <div className="col-md-10">
+				    	<div className="input-group">
+						  detail
+						</div>
+				      
 				    </div>
 				  </div>
 
 				  <div className="form-group">
 				    <div className="col-md-offset-2 col-md-10">
-				      <button type="submit" className="btn btn-primary">Sign in</button>
+				      <button type="submit" className="btn btn-primary">提交</button>
 				    </div>
 				  </div>
 				</form>
