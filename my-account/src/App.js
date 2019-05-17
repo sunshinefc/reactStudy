5d9f0d5a3f0bcc157aaec5ec7b4327fcb0f553de
feicongcong <1075025698@qq.com>
2019-05-17 17:23:12 +0800

    'initbootstrap'

---
+++ my-account/src/App.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import PriceList from './containers/PriceList';
+const items=[
+  {
+    "id":1,
+    "title":"去云南旅游",
+    "price":200,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  },
+  {
+    "id":4,
+    "title":"去云南旅游",
+    "price":400,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  },
+  {
+    "id":3,
+    "title":"去云南旅游",
+    "price":300,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  }
+]
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <PriceList items={items}/>
+    </div>
+  );
+}
+
+export default App;
