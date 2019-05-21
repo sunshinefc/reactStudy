314ad339d7071ade36bdb1749b5bb3b697dc521c
feicongcong <1075025698@qq.com>
2019-05-21 13:38:58 +0800

    'source'

--- my-account/src/App.js
+++ my-account/src/App.js
@@ -1,65 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {LIST_VIEW,CHART_VIEW} from './utility';
-import PriceList from './components/PriceList';
-import ViewTab from './components/ViewTab';
-import MonthPicker from './components/MonthPicker';
-
-const items=[
-  {
-    "id":1,
-    "title":"去云南旅游",
-    "price":200,
-    "date":"2018-09-12",
-    "category":{
-      "id":1,
-      "name":"旅行",
-      "type":"outcome",
-      "iconName":"ios-plane"
-    }
-  },
-  {
-    "id":2,
-    "title":"去云南旅游",
-    "price":400,
-    "date":"2018-09-12",
-    "category":{
-      "id":1,
-      "name":"旅行",
-      "type":"outcome",
-      "iconName":"ios-plane"
-    }
-  },
-  {
-    "id":3,
-    "title":"去云南旅游",
-    "price":300,
-    "date":"2018-09-12",
-    "category":{
-      "id":1,
-      "name":"旅行",
-      "type":"outcome",
-      "iconName":"ios-plane"
-    }
-  }
-]
+import Home from './containers/Home';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>welcome to react</h1>
-        </header>
-        <MonthPicker
-         year={2018}
-         month={5}
-         onChange={(year,month)=>{console.log(year,month)}}/>
-        <ViewTab activeTab={LIST_VIEW}
-          onTabChange={(view)=>{console.log(view)}}/>
+       <Home/>
       </div>
     );
   }
---
+++ my-account/src/containers/Home.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+import {LIST_VIEW,CHART_VIEW,TYPE_INCOME,TYPE_OUTCOME} from '../utility';
+import PriceList from '../components/PriceList';
+import ViewTab from '../components/ViewTab';
+import MonthPicker from '../components/MonthPicker';
+import CreateBtn from '../components/CreateBtn';
+import TotalPrice from '../components/TotalPrice';
+
+const items=[
+  {
+    "id":1,
+    "title":"去云南旅游",
+    "price":200,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  },
+  {
+    "id":2,
+    "title":"去云南旅游",
+    "price":400,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  },
+  {
+    "id":3,
+    "title":"去云南旅游",
+    "price":300,
+    "date":"2018-09-12",
+    "category":{
+      "id":1,
+      "name":"旅行",
+      "type":"outcome",
+      "iconName":"ios-plane"
+    }
+  }
+]
+
+class Home extends React.Component {
+  render(){
+  	let totalIncome=0,totalOutcome=0
+  	items.forEach(item=>{
+  		if(item.category.type===TYPE_OUTCOME){
+  			totalOutcome+=item.price
+  		}else{
+  			totalIncome+=item.price
+  		}
+  	})
+    return (
+    	<React.Fragment>
+	    	<header className="App-header">
+	    		<div className="row mb-5">
+	          		<img src={logo} className="App-logo" alt="logo" />
+	          	</div>
+	          	<div className="row">
+					<div className="col">
+						<MonthPicker 
+							year={2018} 
+							month={8} 
+							onChange={()=>{}}/>
+					</div>
+					<div className="col">
+						<TotalPrice 
+							income={totalIncome}
+							outcome={totalOutcome}
+						/>
+					</div>
+	          	</div>
+	         
+	        </header>
+    	</React.Fragment>
+    );
+  }
+}
+
+export default Home;
