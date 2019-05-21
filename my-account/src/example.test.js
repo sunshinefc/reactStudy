d404abea7088533f0cfdf5bc0f92ac93663583d4
feicongcong <1075025698@qq.com>
2019-05-21 17:20:20 +0800

    'test_jest'

---
+++ my-account/src/example.test.js
@@ -0,0 +1,21 @@
+test('test equal',()=>{
+	expect(2+2).toBe(4)
+})
+
+test('test not equal',()=>{
+	expect(2+2).not.toBe(5)
+})
+
+test('test to be true or false',()=>{
+	expect(1).toBeTruthy()
+	expect(0).toBeFalsy()
+})
+
+test('test number',()=>{
+	expect(4).toBeGreaterThan(3)
+	expect(4).toBeLessThan(5)
+})
+
+test('test object',()=>{
+	expect({name:'viking',age:30}).toEqual({name:'viking',age:30})
+})
